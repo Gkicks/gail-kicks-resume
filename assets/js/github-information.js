70cb782605c563ce0344c7760efdcfb692d9c648
Gkicks <>
2023-11-17 17:17:48 +0000

    fix bugs for the gh-user-data and gh-repo-data elements

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -36,6 +36,10 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+
+    $("#gh-user-data").html = "";
+    $("#gh-repo-data").html = "";
+
     const username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -69,3 +73,4 @@ function fetchGitHubInformation(event) {
         });
 }
 
+$(document).ready(fetchGitHubInformation);
